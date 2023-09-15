e5850888697ffb3d822ae50e8ce19cf6a3424706
dokibacsi <kvalcsi02@gmail.com>
2023-09-15 17:32:11 +0200

    kepek+adatok.js feltoltve

---
+++ adatok.js
@@ -0,0 +1,82 @@
+export const kepekLista = [
+    {
+        kep: "../kepek/53104397377_3a0d857726_c.jpg",
+        auto: "Peugeot 208 Rally2",
+        info: "Ezekkel az autókkal készültek az egyik Peugeot kupára."
+    },
+    {
+        kep: "../kepek/53171139821_0667ac070e_c.jpg",
+        auto: "Hyundai i20",
+        info: "Győr Rally egyik legkomolyabb versenyautója a Rábaringen."
+    },
+    {
+        kep: "../kepek/53171576350_596718ff3b_c.jpg",
+        auto: "Peugeot 208 Rally2",
+        info: "Győr Rally-n verseny a Peugeot kupáért."
+    },
+    {
+        kep: "../kepek/53175917220_85058857e0_c.jpg",
+        auto: "Opel Corsa F",
+        info: "Kerék-Váradi páros iszonyatos fordulása."
+    },
+    {
+        kep: "../kepek/53175917230_02f161e0ee_c.jpg",
+        auto: "Skoda Fabia Rally2",
+        info: "Klausz Kristóf versenyzik a 2023-as HUMDA bajnoki címért."
+    },
+    {
+        kep: "../kepek/ 53177159972_b1064df20f_c.jpg",
+        auto: "Lada",
+        info: "Kishonti célja a nézők balesetmentes szórakoztatása volt."
+    },
+    {
+        kep: "../kepek/53177163727_cf0f14e2ff_c.jpg",
+        auto: "Citroen",
+        info: "Sajnos a srácoknak nem adta ki a vasárnapi futam."
+    },
+    {
+        kep: "../kepek/53177753676_38c58f5642_c.jpg",
+        auto: "Lada",
+        info: "'Túl sok-túl sok!' ~ üvöltötték a szurkolók Virág-Matics párosnak, akik kifaroltak az egyik kanyarban"
+    },
+    {
+        kep: "../kepek/53178187430_b7d547daa7_c.jpg",
+        auto: "Toyota Yaris",
+        info: "Egy kis porolás a Yarissal."
+    },
+    {
+        kep: "../kepek/53178187770_381114f775_c.jpg",
+        auto: "Volkswagen Polo",
+        info: "Turán Frigyes versenyzik a 2023-as HUMDA bajnoki címért. Végül csak a második helyen végzett."
+    },
+    {
+        kep: "../kepek/53178191325_d55db76083_c.jpg",
+        auto: "Hyundai i20",
+        info: "Az egyik legkomolyabban 'festő' autó volt a mezőnyben, bitang hanggal."
+    },
+    {
+        kep: "../kepek/53178246993_3d6aaf30e1_c.jpg",
+        auto: "Porsche 911",
+        info: "A harmadosztály előfutó autója, gyönyörű volt!"
+    },
+    {
+        kep: "../kepek/53179928257_035de738c8_c.jpg",
+        auto: "Citroen WRC",
+        info: "Nagyon komolyakat léptek ezzel a Citroennel is."
+    },
+    {
+        kep: "../kepek/53180517196_83163512b7_c.jpg",
+        auto: "Skoda Fabia",
+        info: "Ezzel az autóval nyerték meg az idei HUMDA országos bajnokságot."
+    },
+    {
+        kep: "../kepek/53180709484_647d250294_c.jpg",
+        auto: "Skoda Fabia",
+        info: "Ezzel a gyönyörű járgánnyal nyerték meg a több mint 30 év után visszatért Győr Rally bajnokságot, 2023-ban."
+    },
+    {
+        kep: "../kepek/53180710574_fb5f47f581_c.jpg",
+        auto: "Subaru Impreza WRX",
+        info: "A szerelemautó, amivel megnyitották a 2023-as Győr Rally bajnokságot"
+    }
+]
